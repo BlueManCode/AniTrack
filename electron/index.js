e79632c493042de2ed0f1b8df2b3356dcbba1413
bluemancode <amanssingh2307@gmail.com>
2021-02-23 11:38:36 +0530

    initial commit for electron app

---
+++ electron/index.js
@@ -0,0 +1,21 @@
+const electron = require('electron')
+const {app, BrowserWindow, globalShortcut} = electron
+
+let main_window;
+let window_options = {
+  resizable: false,
+  frame: false,
+  
+};
+
+// listern for the app to be ready
+app.on('ready', () => {
+  main_window = new BrowserWindow(window_options)
+  main_window.maximize()
+  main_window.hide()
+  main_window.loadURL('https://anitrack.vercel.app/#/')
+  
+  globalShortcut.register('Shift+Enter', () => {
+    main_window.isVisible() ? main_window.hide() : main_window.show()
+  })
+})
